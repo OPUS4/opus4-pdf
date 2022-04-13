152e369b731d4fb3230f64bfbd2e1fd73c1c7f6b
Matthias Steffens <mat@extracts.de>
2022-04-13 10:07:34 +0200

    #452 PDF cover generation is now tested using a proper Opus\Document

--- test/Cover/PdfGenerator/DefaultPdfGeneratorTest.php
+++ test/Cover/PdfGenerator/DefaultPdfGeneratorTest.php
@@ -31,8 +31,14 @@
 
 namespace OpusTest\Pdf\Cover\PdfGenerator;
 
+use DateTime;
+use Opus\Date;
+use Opus\DnbInstitute;
+use Opus\Document;
+use Opus\Identifier;
 use Opus\Pdf\Cover\PdfGenerator\PdfGeneratorFactory;
 use Opus\Pdf\Cover\PdfGenerator\PdfGeneratorInterface;
+use Opus\Person;
 use PHPUnit\Framework\TestCase;
 
 use function file_exists;
@@ -72,15 +78,19 @@ class DefaultPdfGeneratorTest extends TestCase
 
         $generator->setTemplatePath($templatePath);
 
-        // TODO: fix issue if setTempDir() is NOT used: Opus\Exception: Workspace path not found in configuration
         $generator->setTempDir(APPLICATION_PATH . '/test/workspace/tmp/');
 
-        // TODO: provide a proper Document containing metadata (like author, title, abstract, etc)
-        $pdfFilePath = $generator->generateFile(null, 'ifa-cover');
+        // TODO: add dedicated tests that test PDF cover generation with other sample documents (chapter, book, thesis, etc)
+        $document = $this->getSampleArticle();
+
+        $pdfFilePath = $generator->generateFile($document, 'ifa-cover');
 
         // mark output files for deletion
-        $this->tempFiles[] = substr($pdfFilePath, 0, strlen($pdfFilePath) - 4) . '.md';
-        $this->tempFiles[] = $pdfFilePath;
+        $filePathWithoutExtension = substr($pdfFilePath, 0, strlen($pdfFilePath) - 4);
+        $this->tempFiles[]        = $filePathWithoutExtension . '.md';
+        $this->tempFiles[]        = $filePathWithoutExtension . '-csl.json';
+        $this->tempFiles[]        = $filePathWithoutExtension . '-meta.json';
+        $this->tempFiles[]        = $pdfFilePath;
 
         $this->assertNotNull($pdfFilePath);
         $this->assertFileExists($pdfFilePath);
@@ -90,7 +100,7 @@ class DefaultPdfGeneratorTest extends TestCase
      * Returns the full path to the given template.
      *
      * @param string $templateName The template name (or path relative to the 'covers' directory)
-     * @return string Path to template
+     * @return string Path to template.
      */
     private function getTemplatePath($templateName)
     {
@@ -111,5 +121,77 @@ class DefaultPdfGeneratorTest extends TestCase
                 unlink($path);
             }
         }
+    }
+
+    /**
+     * Returns a sample Document object representing an academic journal article.
+     *
+     * @return Document Document object representing an article.
+     */
+    private function getSampleArticle()
+    {
+        $doc = new Document();
+        $doc->store();
+
+        $doc->setType("article");
+        $doc->setLanguage("en");
+
+        $author = new Person();
+        $author->setFirstName('Mats Anders');
+        $author->setLastName('Granskog');
+        $author->setAcademicTitle('Ph.D.');
+        $doc->addPersonAuthor($author);
+
+        $author = new Person();
+        $author->setFirstName('Hermanni');
+        $author->setLastName('Kaartokallio');
+        $doc->addPersonAuthor($author);
+
+        $author = new Person();
+        $author->setFirstName('K');
+        $author->setLastName('Shirasawa');
+        $doc->addPersonAuthor($author);
+
+        $title = $doc->addTitleMain();
+        $title->setValue('Nutrient status of Baltic Sea ice: Evidence for control by snow-ice formation, ice permeability, and ice algae');
+        $title->setLanguage('en');
+
+        $abstract = $doc->addTitleAbstract();
+        $abstract->setValue('Samples of land-fast sea ice collected along the Finnish coast of the Baltic Sea, between latitudes 60.2°N and 65.7°N, in January to April 2000 were analyzed for physical, biological, and chemical parameters. Both spatial and temporal variability were investigated. Snow-ice contributed in average a third of the total ice thickness, while the snow fraction (by mass) of the ice was 20% on average. Snow-ice formation increased the nitrogen concentrations substantially, mainly in the upper parts of the ice cover. Phosphorus on the other hand was controlled by biological uptake, with distinct maxima in the bottommost parts of the ice cover. The chlorophyll-a concentrations were dependent on the physical properties of the ice to some extent. In more saline waters the chlorophyll-a concentrations in the ice were variable (1–17 μg l−1). However, in the less saline waters of the Bothnian Bay the concentrations were generally considerably lower (<1 μg l−1) than elsewhere. This is presumably caused by formation of ice of low salinity, due to the low ambient salinity in the area and the under-ice flow of river waters, and formation of ice that has no habitable space for ice algae. Atmospheric nutrients possibly enhance the magnitude of the ice algae bloom, through downward flushing of surface deposited nutrients during periods when the ice was permeable. We surmise that atmospheric supply of nutrients plays an important role in biological productivity within the Baltic Sea ice sheet and potentially also in under-ice waters.');
+        $abstract->setLanguage('en');
+
+        $doc->setPublishedDate(new Date(new DateTime('2003-08-09')));
+
+        $parent = $doc->addTitleParent();
+        $parent->setValue('Journal of Geophysical Research');
+        $parent->setLanguage('en');
+
+        $doc->setPublisherName('Wiley');
+        $doc->setPublisherPlace('New Jersey');
+
+        $doc->setVolume('108');
+        $doc->setIssue('C8');
+        $doc->setPageFirst(3253);
+        //$doc->setPageLast(3253);
+        $doc->setPageNumber(9);
+
+        $doi = new Identifier();
+        $doi->setType('doi');
+        $doi->setValue('10.1029/2002JC001386');
+
+        $url = new Identifier();
+        $url->setType('url');
+        $url->setValue('https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2002JC001386');
+
+        $issn = new Identifier();
+        $issn->setType('issn');
+        $issn->setValue('0148-0227');
+
+        $ids = [$doi, $url, $issn];
+        $doc->setIdentifier($ids);
+
+        $doc->store();
+
+        return $doc;
     }
 }
